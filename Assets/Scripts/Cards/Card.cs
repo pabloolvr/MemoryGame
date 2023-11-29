e4a5d339b958c991cb75ac58417bab51316f885b
Pablo Oliveira <48020553+pabloolvr@users.noreply.github.com>
2023-11-29 17:37:33 -0300

    Added card spawn and card flip functionality

--- Assets/Scripts/Cards/Card.cs
+++ Assets/Scripts/Cards/Card.cs
@@ -1,18 +1,89 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int Id { get; private set; } = -1;
+    public bool IsUpsideDown { get; private set; }
+
+    public event Action<Card> OnCardFlipped;
+
+    [SerializeField] private GameObject _cardBack;
+    [SerializeField] private TextMeshProUGUI _idField;
+    [SerializeField, Tooltip("Measured in angles per second.")] 
+    private float _flipSpeed = 30f;
+
+    private Button _button;
+
+    private void Awake()
     {
-        
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(() => 
+        {
+            if (IsUpsideDown) Flip();
+        });
+        IsUpsideDown = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Initialize(int id)
     {
-        
+        Id = id;
+        _idField.text = Id.ToString();
+    }
+
+    public void Flip()
+    {
+        OnCardFlipped(this);
+        StartCoroutine(FlipAnimation());
+        Debug.Log($"Card {Id} flipped");
+    }
+
+    private IEnumerator FlipAnimation()
+    {
+        float targetY, newY, curY;
+
+        if (IsUpsideDown)
+        {
+            targetY = 0;
+        }
+        else
+        {
+            targetY = 180;
+        }
+       
+        do
+        {
+            curY = transform.rotation.eulerAngles.y;
+            newY = Mathf.MoveTowards(curY, 90, _flipSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0f, newY, 0f);
+            yield return null;
+        } 
+        //while (Mathf.Abs(newY - targetY) > 0.1f);
+        while (newY != 90);
+
+        _cardBack.SetActive(newY - targetY >= 0);
+
+        do
+        {
+            curY = transform.rotation.eulerAngles.y;
+            newY = Mathf.MoveTowards(curY, targetY, _flipSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0f, newY, 0f);
+            yield return null;
+        }
+        //while (Mathf.Abs(newY - targetY) > 0.1f);
+        while (newY != targetY);
+
+        IsUpsideDown = !IsUpsideDown;
+
+        yield return null;
+    }
+
+    public void SetDiscovered()
+    {
+        _button.interactable = false;
     }
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Card cardPrefab;
+
+    [SerializeField] private int _pairs = 10;
+    [SerializeField] private ScrollRect _cardsContainer;
+
+    //private List<Card> _cards;
+    private Card _firstSelectCard;
+    private Card _secondSelectCard;
+
     void Start()
     {
-        
+        SpawnCards();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SpawnCards()
+    {
+        List<Card> cards = new List<Card>();
+
+        for (int i = 0; i < _pairs; i++)
+        {
+            cards.Add(SpawnCard(i));
+            cards.Add(SpawnCard(i));
+        }
+
+        int cardsCount = cards.Count;
+
+        for (int i = 0; i < cardsCount; i++)
+        {
+            int randomIndex = Random.Range(0, cardsCount - i);
+            cards[randomIndex].transform.SetAsFirstSibling();
+            cards.RemoveAt(randomIndex);
+        }
+    }
+
+    private Card SpawnCard(int index)
+    {
+        Card card = Instantiate(cardPrefab, _cardsContainer.content);
+        card.Initialize(index);
+        card.OnCardFlipped += OnCardFlipped;
+        return card;
+    }
+
+    private void OnCardFlipped(Card card)
+    {
+        //transform.
     }
 }
